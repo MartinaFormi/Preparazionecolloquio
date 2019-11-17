3e0def842aa5a1cef51490f2bcaba24899055154
Martina <martina.formichini@avanade.com>
2019-11-17 15:27:55 +0100

    primo commit

---
+++ Amazon/TodoApi1/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi1.Models
+{
+    public class TodoItem
+    {
+        public int id { get; set; }
+        public int valutazione { get; set; }
+        public int descrizione { get; set; }
+        public int timestamp { get; set; }
+    }
+}
